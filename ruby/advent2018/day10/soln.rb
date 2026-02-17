2ae58cb5c607de8af367d7bd89220ff5a6e5d273
Sarah Llewelyn <se.llewelyn@gmail.com>
2026-02-17 15:00:08 -0500

    part one solution for 2018 day 10

--- ruby/advent2018/day10/soln.rb
+++ ruby/advent2018/day10/soln.rb
@@ -5,39 +5,75 @@ class Solver
     @translations = []
 
     File.foreach('input.txt').with_index do |line, i|
-      x = line[10, 2].to_i
-      y = line[14, 2].to_i
+      x = line[10, 6].to_i
+      y = line[18, 6].to_i
+      # puts "#{x}, #{y}"
       @points << [x, y]
-      xt = line[28, 2].to_i
-      yt = line[32, 2].to_i
+      xt = line[36, 2].to_i
+      yt = line[40, 2].to_i
       @translations << [xt, yt]
+      # puts "#{xt}, #{yt}"
     end
   end
 
   def points_at_time(t)
     # grid = Array.new(200) { Array.new(200) { false } }
     new_points = []
-    min_x = 0
-    min_y = 0
+    h = {}
+    min_x = 1000000
+    min_y = 1000000
     max_x = 0
     max_y = 0
     @points.each_with_index do |(x, y), i|
       xp = (x + (@translations[i][0] * t))
       yp = (y + (@translations[i][1] * t))
       new_points << [xp, yp]
+      # get row
+      if h[yp]
+        h[yp][xp] = true
+      else
+        h[yp] = { xp => true }
+      end
       min_x = xp if xp < min_x
       min_y = yp if yp < min_y
       max_x = xp if xp > max_x
       max_y = yp if yp > max_y
     end
 
-    grid = Array.new(max_x - min_x + 1) { Array.new(max_y - min_y + 1) { false } }
-    new_points.each do |(x, y)|
-      grid[x -min_x][y - min_y] = true
+    # puts h.inspect
+
+    (min_y..max_y).each do |y|
+      row = h[y]
+      if row.nil?
+        puts "*"
+        next
+      end
+      str = "*"
+      (min_x..max_x).each do |x|
+        if row[x]
+          str += "#"
+        else
+          str += "_"
+        end
+
+      end
+      puts str
+
     end
 
-    print_grid(grid.transpose)
+    puts
+
+    puts "#{max_x - min_x}  #{max_y - min_y}"
+
+    # grid = Array.new(max_x - min_x + 1) { Array.new(max_y - min_y + 1) { false } }
+    # new_points.each do |(x, y)|
+    #   grid[x -min_x][y - min_y] = true
+    # end
+    #
+    # print_grid(grid.transpose)
   end
+
+  def print_h(h) end
 
   def print_grid(grid)
     grid.each do |row|
@@ -56,4 +92,4 @@ class Solver
 end
 
 solver = Solver.new
-solver.points_at_time(3)
+solver.points_at_time(10011)
