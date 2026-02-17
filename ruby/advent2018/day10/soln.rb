45b13b22d11d9b3cf2cfaf1546c24ee476d1d602
Sarah Llewelyn <se.llewelyn@gmail.com>
2026-02-17 14:31:36 -0500

    soln 2018 10 works for toy example

---
+++ ruby/advent2018/day10/soln.rb
@@ -0,0 +1,59 @@
+class Solver
+
+  def initialize
+    @points = []
+    @translations = []
+
+    File.foreach('input.txt').with_index do |line, i|
+      x = line[10, 2].to_i
+      y = line[14, 2].to_i
+      @points << [x, y]
+      xt = line[28, 2].to_i
+      yt = line[32, 2].to_i
+      @translations << [xt, yt]
+    end
+  end
+
+  def points_at_time(t)
+    # grid = Array.new(200) { Array.new(200) { false } }
+    new_points = []
+    min_x = 0
+    min_y = 0
+    max_x = 0
+    max_y = 0
+    @points.each_with_index do |(x, y), i|
+      xp = (x + (@translations[i][0] * t))
+      yp = (y + (@translations[i][1] * t))
+      new_points << [xp, yp]
+      min_x = xp if xp < min_x
+      min_y = yp if yp < min_y
+      max_x = xp if xp > max_x
+      max_y = yp if yp > max_y
+    end
+
+    grid = Array.new(max_x - min_x + 1) { Array.new(max_y - min_y + 1) { false } }
+    new_points.each do |(x, y)|
+      grid[x -min_x][y - min_y] = true
+    end
+
+    print_grid(grid.transpose)
+  end
+
+  def print_grid(grid)
+    grid.each do |row|
+      str = ""
+      row.each do |cell|
+        if cell
+          str += "#"
+        else
+          str += "_"
+        end
+      end
+      puts str
+    end
+  end
+
+end
+
+solver = Solver.new
+solver.points_at_time(3)
